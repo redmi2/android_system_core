b3e806026b58de204084b66de754060ead010d66
Praveen Chidambaram <pchidamb@quicinc.com>
2009-05-20 08:26:36 -0600

    system/core : Add 8k OnDemand governor preferences to init.qcom.sh
    Set up threshold to 90% CPU utilization
    Set down differentail to 30% so that the down threshold is 60%
    Set sampling rate to 500 ms

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -50,5 +50,8 @@ case "$target" in
                 echo $value > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                 ;;
         esac
+        echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
+        echo 30 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/down_differential
+        echo 500000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
         ;;
 esac
