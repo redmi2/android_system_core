200c8957d0c95bd0fe8c5a631f233ef886279880
Amar Singhal <c_amars@quicinc.com>
2010-01-22 17:23:55 -0800

    system/core: changing the value of AID_NET_ADMIN to that used in kernel and google aosp

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -63,8 +63,9 @@
 #define AID_NET_BT        3002  /* bluetooth: create sco, rfcomm or l2cap sockets */
 #define AID_INET          3003  /* can create AF_INET and AF_INET6 sockets */
 #define AID_NET_RAW       3004  /* can create raw INET sockets */
-#define AID_QCOM_ONCRPC   3005  /* can read/write /dev/oncrpc/* */
-#define AID_NET_ADMIN     3006  /* can configure interfaces and routing tables. */
+#define AID_NET_ADMIN     3005  /* can configure interfaces and routing tables. */
+#define AID_QCOM_ONCRPC   3006  /* can read/write /dev/oncrpc/* */
+
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
