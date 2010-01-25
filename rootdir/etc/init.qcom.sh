0c8ae24b1997786a4b1edbb1dffc120fe1f0d321
Karthik Parsha <kparsha@codeaurora.org>
2010-01-25 10:10:16 -0800

    init.qcom.sh: Plugin Unstable Memory if available.
    if the sys file 'memory_start_bytes' is available and has a valid address
    value, then attempt
    a.Physical Hotplug
    b.Logical Hotplug
    c.Set DMM-DPD related Android System properties.
    
    Change-Id: I7550b7fd67e2e19bb77589c1194390e2d6fa4f8c

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -52,6 +52,79 @@ case "$target" in
              ln -s  /system/usr/keychars/surf_keypad_qwerty.kcm.bin /system/usr/keychars/surf_keypad.kcm.bin;;
 
         esac
+
+# Dynamic Memory Managment (DMM) provides a sys file system to the userspace
+# that can be used to plug in/out memory that has been configured as unstable.
+# This unstable memory can be in Active or In-Active State.
+# Each of which the userspace can request by writing to a sys file.
+
+# ro.dev.dmm = 1; Indicates that DMM is enabled in the Android User Space. This
+# property is set in the Android system properties file.
+
+# ro.dev.dmm.dpd.start_address is set when the target has a 2x256Mb memory
+# configuration. This is also used to indicate that the target is capable of
+# setting EBI-1 to Deep Power Down or Self Refresh.
+
+        mem="/sys/devices/system/memory"
+        op=`cat $mem/movable_start_bytes`
+        case "$op" in
+           "0" )
+                log -p i -t DMM DMM Disabled. movable_start_bytes not set: $op
+            ;;
+
+            "$mem/movable_start_bytes: No such file or directory " )
+                log -p i -t DMM DMM Disabled. movable_start_bytes does not exist: $op
+            ;;
+
+            * )
+                log -p i -t DMM DMM available. movable_start_bytes at $op
+                movable_start_bytes=0x`cat $mem/movable_start_bytes`
+                block_size_bytes=0x`cat $mem/block_size_bytes`
+                block=$(($movable_start_bytes/$block_size_bytes))
+
+                echo $movable_start_bytes > $mem/probe
+                case "$?" in
+                    "0" )
+                        log -p i -t DMM $movable_start_bytes to physical hotplug succeeded.
+                    ;;
+                    * )
+                        log -p e -t DMM $movable_start_bytes to physical hotplug failed.
+                        return 1
+                    ;;
+                esac
+
+               chown system system $mem/memory$block/state
+
+                echo online > $mem/memory$block/state
+                case "$?" in
+                    "0" )
+                        log -p i -t DMM \'echo online\' to logical hotplug succeeded.
+                    ;;
+                    * )
+                        log -p e -t DMM \'echo online\' to logical hotplug failed.
+                        return 1
+                    ;;
+                esac
+
+                setprop ro.dev.dmm.dpd.start_address $movable_start_bytes
+                setprop ro.dev.dmm.dpd.block $block
+            ;;
+        esac
+
+        op=`cat $mem/low_power_memory_start_bytes`
+        case "$op" in
+            "0" )
+                log -p i -t DMM Self-Refresh-Only Disabled. low_power_memory_start_bytes not set:$op
+            ;;
+
+            "$mem/low_power_memory_start_bytes No such file or directory " )
+                log -p i -t DMM Self-Refresh-Only Disabled. low_power_memory_start_bytes does not exist:$op
+            ;;
+
+            * )
+                log -p i -t DMM Self-Refresh-Only available. low_power_memory_start_bytes at $op
+            ;;
+        esac
         ;;
 
     "msm8660_surf")
