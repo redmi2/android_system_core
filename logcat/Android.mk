d1fe13bb07684f969c1aaff8dc40bbee3a32924e
Vasantha Kumar <vasant@codeaurora.org>
2010-07-15 00:01:14 -0700

    logcat: Add diag support to logcat.
    Add diag support to logcat to route logcat messages to QXDM.
    
    Change-Id: I573f7792e8130f9b22dcc0a97f7dad392c653ffd
    CRs-fixed: 230615

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -7,6 +7,12 @@ LOCAL_SRC_FILES:= logcat.cpp event.logtags
 
 LOCAL_SHARED_LIBRARIES := liblog
 
+ifeq ($(strip $(QC_PROP)),true)
+LOCAL_SHARED_LIBRARIES += libdiag
+LOCAL_CFLAGS += -DUSE_DIAG
+LOCAL_C_INCLUDES := vendor/qcom/opensource/diag
+endif # QC_PROP
+
 LOCAL_MODULE:= logcat
 
 include $(BUILD_EXECUTABLE)
