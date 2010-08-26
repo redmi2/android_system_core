5619c16a5c45c9eb6478b2f1571d4c4ce9fa2b18
Susheel Yadagiri <syadagir@codeaurora.org>
2010-08-26 15:24:54 -0700

    system/core: Launch profiler daemon.
    This profiler daemon collects CPU, network, power
    statistics. It is used by the Trepn application.
    
    Change-Id: I33a8b55a24934d1d8d7b5e6c1ac9600d4510d4fb

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -47,7 +47,8 @@ case "$target" in
             "FFA" | "SVLTE_FFA")
              ln -s  /system/usr/keychars/surf_keypad_numeric.kcm.bin /system/usr/keychars/surf_keypad.kcm.bin;;
             "Fluid")
-             setprop ro.sf.lcd_density 240;;
+             setprop ro.sf.lcd_density 240
+             /system/bin/profiler_daemon;;
             *)
              ln -s  /system/usr/keychars/surf_keypad_qwerty.kcm.bin /system/usr/keychars/surf_keypad.kcm.bin;;
 
