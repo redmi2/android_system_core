d88d4cae2c2fd030002fb926314fd200fdef5b52
Kevin DuBois <kdubois@codeaurora.org>
2010-11-01 09:55:04 -0700

    system/core: add permission for second 2d graphics core
    Change-Id: Icd6a32c28adf4f5965e597db78d9c03a252af5c9

--- init/devices.c
+++ init/devices.c
@@ -107,6 +107,7 @@ static struct perms_ devperms[] = {
     /* gpu driver for adreno200 is globally accessible */
     { "/dev/kgsl-3d0",      0666,   AID_ROOT,       AID_ROOT,       0 },
     { "/dev/kgsl-2d0",      0666,   AID_ROOT,       AID_ROOT,       0 },
+    { "/dev/kgsl-2d1",      0666,   AID_ROOT,       AID_ROOT,       0 },
 
         /* these should not be world writable */
     { "/dev/diag_arm9",     0660,   AID_RADIO,      AID_RADIO,        0 },
