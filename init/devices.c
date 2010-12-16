42026465fb0bb250dd4d5e6e048dae4f793ee794
Shivaprasad Hongal <shivaprasadhongal@codeaurora.org>
2010-12-16 12:32:33 -0800

    device.c: Changed the permissions of /dev/radio0
    Changed the permissions of /dev/radio0 from 0644 to 0640.
    
    Change-Id: Ic43c858513ef6037244f87579bb18c80d0e1bc05
    CRs-fixed: 268055

--- init/devices.c
+++ init/devices.c
@@ -143,7 +143,7 @@ static struct perms_ devperms[] = {
     { "/dev/cm3602",        0640,   AID_COMPASS,    AID_SYSTEM,     0 },
     { "/dev/akm8976_pffd",  0640,   AID_COMPASS,    AID_SYSTEM,     0 },
     { "/dev/lightsensor",   0640,   AID_SYSTEM,     AID_SYSTEM,     0 },
-    { "/dev/radio0",        0644,   AID_FM_RADIO,   AID_FM_RADIO,   1 },
+    { "/dev/radio0",        0640,   AID_FM_RADIO,   AID_FM_RADIO,   1 },
 #ifdef AUDIOV2
     { "/dev/msm_pcm_lp_dec",   0666,   AID_SYSTEM,     AID_AUDIO,      1 },
     { "/dev/snd/controlC0", 0666,   AID_SYSTEM,     AID_AUDIO,      1 },
