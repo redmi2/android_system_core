ec656ef6a8c13bf4666aea67196323ad047d91c1
Jeffrey Chuang <fchuang@codeaurora.org>
2010-12-21 11:59:36 -0800

    System: core: rootdir: etc: init.qcom.post_boot.sh: disable power features
    Disable power collapse, L2_cache, pxo, vdd_dig, and vdd_mem
    
    Change-Id: I1a80c815e11f4a7572845266e2de36a55a48934c

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -61,18 +61,18 @@ case "$target" in
 #        echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 #        echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
 #        echo 500000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_dig
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_mem
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
+#	 echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
+#	 echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
+#	 echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_dig
+#	 echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_mem
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
+#	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
         ;;
 esac
 
