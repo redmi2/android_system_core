c0dcdade8cbad143c084fb175e23ac192d2d5035
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2011-01-14 15:23:40 -0800

    system/core: init.qcom.sh: Allow charging disabling to persist
    Have the init process check for property persist.usb.chgdisabled
    and use it to disable charging at bootup.
    
    Steps to disable charging:
    boot phone
    adb shell setprop persist.usb.chgdisabled 1
    reboot phone
    Booted phone will now have usb charging disabled
    
    Steps to enable charging:
    boot phone
    adb shell setprop persist.usb.chgdisabled 0
    reboot phone
    Booted phone will allow charging from usb
    
    Change-Id: Ie87ca5a4e62b2380181bad938f33ec0148422455

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -38,7 +38,21 @@ case "$serialno" in
     echo "$serialno" > /sys/kernel/debug/android/serial_number
 esac
 
+#
+# Allow persistent usb charging disabling
+# User needs to set usb charging disabled in persist.usb.chgdisabled
+#
 target=`getprop ro.product.device`
+usbchgdisabled=`getprop persist.usb.chgdisabled`
+case "$usbchgdisabled" in
+    "") ;; #Do nothing here
+    * )
+    case $target in
+        "msm8660_surf" | "msm8660_csfb")
+        echo "$usbchgdisabled" > /sys/module/pmic8058_charger/parameters/disabled
+    esac
+esac
+
 case "$target" in
     "msm7630_surf" | "msm7630_1x" | "msm7630_fusion")
         insmod /system/lib/modules/ss_mfcinit.ko
