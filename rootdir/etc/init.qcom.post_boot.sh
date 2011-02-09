7a825bb0ab3ea0f7beaa5ae9c9465d1e0fd9a9ec
Jessica Gonzalez <jgaona@codeaurora.org>
2011-02-09 15:51:47 -0800

    Change ownership of cpufreq/ondemand/sampling_rate to system for 8660.
    This allows the PowerManagerService to set the sampling_rate
    higher when the display turns off and vice versa
    
    (cherry picked from commit 9c23314c85bad383fcc7f2ac244cab9ad168be26)
    
    Change-Id: I80c79ea04941ab3acc9fb139b3fe066426d3110c

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+# Copyright (c) 2008-2010, Code Aurora Forum. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -74,6 +74,8 @@ case "$target" in
 	 echo 50000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
 	 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
 	 echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
+	 chown system /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
+	 chown system /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
         ;;
 esac
 
