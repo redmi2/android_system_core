64ff7087b855af94295ef51d33627e07512d9995
Jessica Gonzalez <jgaona@codeaurora.org>
2011-02-09 16:10:29 -0800

    system/core: adding more default parameters to the ondemand governor
    (cherry picked from commit 0aee6c78003af08d11c0b36ffa148fe0c265d6ce)
    
    Change-Id: I157e1f27ed070a83cb08bb8a06c9d6f89a4bc148

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -74,7 +74,8 @@ case "$target" in
 	 echo 50000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
 	 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
 	 echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
-         echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
 	 chown system /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
         ;;
