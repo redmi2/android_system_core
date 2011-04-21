ec0b4297165328226e007a3b0124070c223eafea
Jessica Gonzalez <jgaona@codeaurora.org>
2011-04-21 11:32:13 -0700

    rootdir: Set permissions of efs sync sysfs nodes to system/system
    During graceful shutdown of emmc devices, we need to force modem
    to sync its file system.  This is done using sysfs nodes.  Set the
    permissions of these sysfs nodes so Android framework can initiate
    force efs sync on shutdown.
    
    (cherry picked from commit 5d1da565eeb35a72ab455af4037a925bb4e1e620)
    
    Conflicts:
    
    	rootdir/etc/init.qcom.post_boot.sh
    
    Change-Id: Ib00378c8690f6d5b7012235cf28ea72cc719e3a3

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -108,3 +108,12 @@ esac
 
 chown system /sys/devices/system/cpu/mfreq
 chown system /sys/devices/system/cpu/cpu1/online
+emmc_boot=`getprop ro.emmc`
+case "$emmc_boot"
+    in "1")
+        chown system /sys/devices/platform/rs300000a7.65536/force_sync
+        chown system /sys/devices/platform/rs300000a7.65536/sync_sts
+        chown system /sys/devices/platform/rs300100a7.65536/force_sync
+        chown system /sys/devices/platform/rs300100a7.65536/sync_sts
+    ;;
+esac
