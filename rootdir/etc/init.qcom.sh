35ad1926a13efe4fdda46b2d917931ebf0794e22
Chiranjeevi Velempati <cvelempa@codeaurora.org>
2011-12-28 10:34:41 +0530

    system/core: Fix race condition issue for serial number update
    USB serial number should be always update before USB enumeration.
    At times, serial number is getting updated only after USB enumeration,
    due to uncertainity involved in the order of execution of init scripts.
    
    Fix this issue, by writing the serial number just before USB enumeration.
    
    CRs-Fixed: 324997
    Change-Id: Ia37f4c2411a9d1a1c4748bcb9566e47d117bc60a

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -84,94 +84,6 @@ do
 done
 
 #
-# Update USB serial number if passed from command line
-#
-serialnum=`getprop ro.serialno`
-case "$serialnum" in
-    "") ;; #Do nothing, use default serial number or check for persist one below
-    * )
-    echo "$serialnum" > /sys/class/android_usb/android0/iSerial
-esac
-
-#
-# Allow unique persistent serial numbers for devices connected via usb
-# User needs to set unique usb serial number to persist.usb.serialno
-#
-serialno=`getprop persist.usb.serialno`
-case "$serialno" in
-    "") ;; #Do nothing here
-    * )
-    echo "$serialno" > /sys/class/android_usb/android0/iSerial
-esac
-
-#
-# Allow persistent usb charging disabling
-# User needs to set usb charging disabled in persist.usb.chgdisabled
-#
-target=`getprop ro.board.platform`
-usbchgdisabled=`getprop persist.usb.chgdisabled`
-case "$usbchgdisabled" in
-    "") ;; #Do nothing here
-    * )
-    case $target in
-        "msm8660")
-        echo "$usbchgdisabled" > /sys/module/pmic8058_charger/parameters/disabled
-        echo "$usbchgdisabled" > /sys/module/smb137b/parameters/disabled
-	;;
-        "msm8960")
-        echo "$usbchgdisabled" > /sys/module/pm8921_charger/parameters/disabled
-	;;
-    esac
-esac
-
-#
-# Allow USB enumeration with default PID/VID
-#
-echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
-usb_config=`getprop persist.sys.usb.config`
-case "$usb_config" in
-    "" | "adb") #USB persist config not set, select default configuration
-        case $target in
-            "msm8960")
-                socid=`cat /sys/devices/system/soc/soc0/id`
-                case "$socid" in
-                    "109")
-                         setprop persist.sys.usb.config diag,adb
-                    ;;
-                    *)
-                        case "$baseband" in
-                            "mdm")
-                                 setprop persist.sys.usb.config diag,diag_mdm,serial_hsic,serial_tty,rmnet_hsic,mass_storage,adb
-                            ;;
-                            *)
-                                 setprop persist.sys.usb.config diag,serial_smd,serial_tty,rmnet_bam,mass_storage,adb
-                            ;;
-                        esac
-                    ;;
-                esac
-            ;;
-            "msm7627a")
-                setprop persist.sys.usb.config diag,serial_smd,serial_tty,rmnet_smd,mass_storage,adb
-            ;;
-            * )
-                case "$baseband" in
-                    "svlte2a")
-                         setprop persist.sys.usb.config diag,diag_mdm,serial_sdio,serial_smd,rmnet_smd_sdio,mass_storage,adb
-                    ;;
-                    "csfb")
-                         setprop persist.sys.usb.config diag,diag_mdm,serial_sdio,serial_tty,rmnet_sdio,mass_storage,adb
-                    ;;
-                    *)
-                         setprop persist.sys.usb.config diag,serial_tty,serial_tty,rmnet_smd,mass_storage,adb
-                    ;;
-                esac
-            ;;
-        esac
-    ;;
-    * ) ;; #USB persist config exists, do nothing
-esac
-
-#
 # Start gpsone_daemon for SVLTE Type I & II devices
 #
 case "$target" in
@@ -300,20 +212,12 @@ case "$target" in
             "Dragon")
                 setprop ro.sound.alsa "WM8903";;
         esac
-        chown root.system /sys/devices/platform/msm_hsusb/gadget/wakeup
-        chmod 220 /sys/devices/platform/msm_hsusb/gadget/wakeup
-        ;;
-    "msm7627a" )
-        chown root.system /sys/devices/platform/msm_hsusb/gadget/wakeup
-        chmod 220 /sys/devices/platform/msm_hsusb/gadget/wakeup
         ;;
     "msm8960")
         case "$baseband" in
             "msm")
                 start_sensors;;
         esac
-        chown root.system /sys/devices/platform/msm_otg/msm_hsusb/gadget/wakeup
-        chmod 220 /sys/devices/platform/msm_otg/msm_hsusb/gadget/wakeup
 
         # Dynamic Memory Managment (DMM) provides a sys file system to the userspace
         # that can be used to plug in/out memory that has been configured as 'Movable'.
@@ -351,10 +255,6 @@ case "$target" in
                 setprop ro.dev.dmm.dpd.block $block
             ;;
         esac
-        ;;
-    "msm7630_surf" )
-        chown root.system /sys/devices/platform/msm_hsusb/gadget/wakeup
-        chmod 220 /sys/devices/platform/msm_hsusb/gadget/wakeup
         ;;
 
 esac
