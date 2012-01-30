92eb5604105e0f32687edea818c460e22ba6b5a4
Veena Sambasivan <veenas@codeaurora.org>
2012-01-29 23:57:07 -0800

    Modifying down_differential to 10
    Change-Id: Ia0be21f7e571fa474f0507ed1551c1ad64689aab

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -90,6 +90,7 @@ case "$target" in
 	 echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
 	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+         echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
 	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
@@ -129,6 +130,7 @@ case "$target" in
          echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
          echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
          echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+         echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
          echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
