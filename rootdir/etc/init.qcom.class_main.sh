cdc9950744be054ea7812750fe0c326b4fe571d6
Shashank Mittal <mittals@codeaurora.org>
2012-05-01 13:54:30 -0700

    init: qcom: Move all telephony services to main class.
    Change-Id: Iecfb8791ecd2e9fae72d3e62c8fc0138f57e6f92

--- rootdir/etc/init.qcom.class_main.sh
+++ rootdir/etc/init.qcom.class_main.sh
@@ -25,3 +25,37 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+#
+# start ril-daemon only for targets on which radio is present
+#
+baseband=`getprop ro.baseband`
+multirild=`getprop ro.multi.rild`
+dsds=`getprop persist.dsds.enabled`
+netmgr=`getprop ro.use_data_netmgrd`
+
+case "$baseband" in
+    "apq")
+    setprop ro.radio.noril yes
+    stop ril-daemon
+esac
+
+case "$baseband" in
+    "msm" | "csfb" | "svlte2a" | "mdm"| "unknown")
+    start qmuxd
+    case "$baseband" in
+        "svlte2a" | "csfb")
+        start qmiproxy
+    esac
+    case "$multirild" in
+        "true")
+         case "$dsds" in
+             "true")
+             start ril-daemon1
+         esac
+    esac
+    case "$netmgr" in
+        "true")
+        start netmgrd
+    esac
+esac
--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -58,39 +58,7 @@ start_battery_monitor()
 	start battery_monitor
 }
 
-#
-# start ril-daemon only for targets on which radio is present
-#
 baseband=`getprop ro.baseband`
-multirild=`getprop ro.multi.rild`
-dsds=`getprop persist.dsds.enabled`
-netmgr=`getprop ro.use_data_netmgrd`
-
-case "$baseband" in
-    "apq")
-    setprop ro.radio.noril yes
-    stop ril-daemon
-esac
-
-case "$baseband" in
-    "msm" | "csfb" | "svlte2a" | "mdm"| "unknown")
-    start qmuxd
-    case "$baseband" in
-        "svlte2a" | "csfb")
-        start qmiproxy
-    esac
-    case "$multirild" in
-        "true")
-         case "$dsds" in
-             "true")
-             start ril-daemon1
-         esac
-    esac
-    case "$netmgr" in
-        "true")
-        start netmgrd
-    esac
-esac
 
 #
 # Suppress default route installation during RA for IPV6; user space will take
