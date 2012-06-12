0a377f2c3e7fb624df1b61a1f78db9fceef2a47c
Shashank Mittal <mittals@codeaurora.org>
2012-06-11 18:37:29 -0700

    init: qcom: Move all telephony services to main class.
    Change-Id: Iecfb8791ecd2e9fae72d3e62c8fc0138f57e6f92
    
    Conflicts:
    
    	rootdir/etc/init.qcom.sh

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -48,32 +48,7 @@ start_sensors()
     start sensors
 }
 
-# start ril-daemon only for targets on which radio is present
-#
 baseband=`getprop ro.baseband`
-multirild=`getprop ro.multi.rild`
-dsds=`getprop persist.dsds.enabled`
-netmgr=`getprop ro.use_data_netmgrd`
-case "$baseband" in
-    "msm" | "csfb" | "svlte2a" | "mdm" | "unknown")
-    start ril-daemon
-    start qmuxd
-    case "$baseband" in
-        "svlte2a" | "csfb")
-        start qmiproxy
-    esac
-    case "$multirild" in
-        "true")
-         case "$dsds" in
-             "true")
-             start ril-daemon1
-         esac
-    esac
-    case "$netmgr" in
-        "true")
-        start netmgrd
-    esac
-esac
 
 #
 # Suppress default route installation during RA for IPV6; user space will take
