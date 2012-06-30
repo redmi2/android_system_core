ffb3b5f088290201776cdf80a66d0ad9d3a1b37d
Dixon Peterson <dixonp@codeaurora.org>
2012-06-29 17:19:17 -0700

    diag: Remove world read/write permissions from /dev/diag
    /dev/diag has world read/write permissions which is considered
    a security risk.  Remove these permissions.  Add new qcom_diag
    group for access to /dev/diag
    
    CRs-Fixed: 356415
    (cherry picked from commit 14c85ec56fd47c5eb143617a3bd5a6e7ee39cb6e)
    Change-Id: Ia261176e5ee75cc5dda7f188fda667582142196e

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -77,7 +77,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
-#define AID_QCOM_DIAG     3009 /* can read/write /dev/diag */
+#define AID_QCOM_DIAG     3009  /* can read/write /dev/diag */
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
