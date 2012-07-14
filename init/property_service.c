1e2e86ac3c85b648de645dd40c13c6cdf02f21e4
Deepthi Gowri <deepthi@codeaurora.org>
2012-07-15 00:32:15 +0530

    init: Add wifi.wpa_supp.status property.
    This property is used to avoid ANR which try to access
    wpa_supplicant before wpa_supplicant is fully initilized
    and ready.
    
    Change-Id: Ie7841714b8b63ce2cb74dde0a019202c9a48c682

--- init/property_service.c
+++ init/property_service.c
@@ -87,6 +87,7 @@ struct {
     { "persist.service.", AID_SYSTEM,   0 },
     { "persist.security.", AID_SYSTEM,   0 },
     { "bluetooth.",       AID_SYSTEM,    0 },
+    { "wifi.",            AID_WIFI,     0 },
     { NULL, 0, 0 }
 };
 
