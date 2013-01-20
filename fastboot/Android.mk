9b4b86216e3c69f1d3f032da6226d21f5bf572ce
Tom Mackall <tmackall@codeaurora.org>
2013-01-20 12:22:54 -0800

    Revert "Update copyright to The Linux Foundation"
    This reverts commit 5fe9bb34859d137b0163f8931acad9310b57538d
    
    Change-Id: Ibfc0e447f0b0462ebc8bf4d6b8fd2b0d7ad361ca

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2007 Google Inc.
-# Copyright (c) 2009, The Linux Foundation. All rights reserved.
+# Copyright (c) 2009, Code Aurora Forum. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,5 +1,5 @@
 // Copyright 2006 The Android Open Source Project
-/* Copyright (c) 2009, The Linux Foundation.
+/* Copyright (c) 2009, Code Aurora Forum.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
