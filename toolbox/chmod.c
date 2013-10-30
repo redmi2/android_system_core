90e93e7a92212c70ed20f7ef322df6681f5c8dc9
Biswajit Paul <biswajitpaul@codeaurora.org>
2013-10-29 20:03:59 -0700

    Added No Follow symlink option to chown and chmod
    Enable symlink to not follow target file if "-h" option is
    provided.
    
    CRs-fixed: 545883
    Change-Id: Iefd490bdd12cbe4cf9011f4b9b5e217493b2b503

--- toolbox/chmod.c
+++ toolbox/chmod.c
@@ -7,16 +7,55 @@
 
 #include <unistd.h>
 #include <time.h>
+#include <fcntl.h>
+#include <getopt.h>
+
+
+static int usage()
+{
+    fprintf(stderr, "Usage: chmod [OPTION] <MODE> <FILE>\n");
+    fprintf(stderr, "  -h, --no-dereference    do not follow symlink\n");
+    fprintf(stderr, "  --help                  display this help and exit\n");
+
+    return 10;
+}
 
 int chmod_main(int argc, char **argv)
 {
     int i;
-
-    if (argc < 3) {
-        fprintf(stderr, "Usage: chmod <MODE> <FILE>\n");
-        return 10;
+    int noFollow = 0;
+    int fd = 0;
+    int ch = 0;
+    unsigned int flag =0;
+    static struct option long_options[] =
+        {
+            {"help",       no_argument,       0, 'H'},
+            {"no-dereference",  no_argument,  0, 'h'}
+        };
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+    while((ch = getopt_long(argc, argv, "Hh",long_options,&option_index)) != -1)
+    switch(ch){
+        case 'H':
+            if(argc < 3)
+                return usage();
+            break;
+        case 'h':
+            noFollow = 1;
+            break;
+        default:
+            break;
     }
 
+    if (noFollow && argc < 4) {
+        return usage();
+    }
+
+    if(noFollow) {
+        flag = O_NOFOLLOW;
+        argc--;
+        argv++;
+    }
     int mode = 0;
     const char* s = argv[1];
     while (*s) {
@@ -30,11 +69,17 @@ int chmod_main(int argc, char **argv)
         s++;
     }
     for (i = 2; i < argc; i++) {
-        if (chmod(argv[i], mode) < 0) {
-            fprintf(stderr, "Unable to chmod %s: %s\n", argv[i], strerror(errno));
+        if(((fd = open(argv[i], flag|O_RDONLY )) != -1)||((fd = open(argv[i], flag|O_WRONLY )) != -1)) {
+            if (fchmod(fd, mode) < 0){
+                fprintf(stderr, "Unable to chmod %s: %s\n", argv[i], strerror(errno));
+                close(fd);
+                return 10;
+            }
+            close(fd);
+        } else {
+            fprintf(stderr, "Unable to open %s: %s\n", argv[i], strerror(errno));
             return 10;
         }
     }
     return 0;
 }
-
