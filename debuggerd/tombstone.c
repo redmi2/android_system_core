0c81b1334e91761f7726b5ea23442e4a1b892efd
Biswajit Paul <biswajitpaul@codeaurora.org>
2014-02-18 12:03:44 -0800

    Make tombstones directory to not follow symlink.
    Use file descriptor to change ownership.
    
    Change-Id: Ie6919c549885d950975a003c4009b6e185df6e64
    CRs-Fixed: 600072

--- debuggerd/tombstone.c
+++ debuggerd/tombstone.c
@@ -681,7 +681,7 @@ static char* find_and_open_tombstone(int* fd)
         if (errno != ENOENT)
             continue;
 
-        *fd = open(path, O_CREAT | O_EXCL | O_WRONLY, 0600);
+        *fd = open(path, O_CREAT | O_EXCL | O_WRONLY | O_NOFOLLOW, 0600);
         if (*fd < 0)
             continue;   /* raced ? */
 
@@ -704,7 +704,18 @@ char* engrave_tombstone(pid_t pid, pid_t tid, int signal,
         bool dump_sibling_threads, bool quiet, bool* detach_failed,
         int* total_sleep_time_usec) {
     mkdir(TOMBSTONE_DIR, 0755);
-    chown(TOMBSTONE_DIR, AID_SYSTEM, AID_SYSTEM);
+    int fd;
+    if(((fd = open(TOMBSTONE_DIR, O_NOFOLLOW|O_RDONLY)) != -1) ||((fd = open(TOMBSTONE_DIR, O_NOFOLLOW|O_WRONLY)) != -1)){
+        if (fchown(fd, AID_SYSTEM, AID_SYSTEM) < 0){
+            fprintf(stderr, "Unable to chown %s: %s\n", TOMBSTONE_DIR, strerror(errno));
+            close(fd);
+            return NULL;
+        }
+        close(fd);
+    } else {
+            fprintf(stderr, "Unable to open %s: %s\n", TOMBSTONE_DIR, strerror(errno));
+            return NULL;
+    }
 
 #ifdef HAVE_SELINUX
     if (selinux_android_restorecon(TOMBSTONE_DIR) == -1) {
@@ -713,7 +724,6 @@ char* engrave_tombstone(pid_t pid, pid_t tid, int signal,
     }
 #endif
 
-    int fd;
     char* path = find_and_open_tombstone(&fd);
     if (!path) {
         *detach_failed = false;
