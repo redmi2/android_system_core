db6cde1822d5bcdd847098cd2fd2fdf970a12797
Deven Patel <cdevenp@codeaurora.org>
2015-10-28 11:59:25 -0700

    system: core: update list header file to the latest
    Latest list header file is required for audio module.
    
    Change-Id: Ic58fea54c38ee55782720b0f6d2d07ffaacb5a7d
    Signed-off-by: Deven Patel <cdevenp@codeaurora.org>

--- adb/utils.c
+++ adb/utils.c
@@ -107,23 +107,3 @@ buff_add  (char*  buff, char*  buffEnd, const char*  format, ... )
     return buff;
 }
 
-void list_init(struct listnode *node)
-{
-    node->next = node;
-    node->prev = node;
-}
-
-void list_add_tail(struct listnode *head, struct listnode *item)
-{
-    item->next = head;
-    item->prev = head->prev;
-    head->prev->next = item;
-    head->prev = item;
-}
-
-void list_remove(struct listnode *item)
-{
-    item->next->prev = item->prev;
-    item->prev->next = item->next;
-}
-
