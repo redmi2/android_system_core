8205b85fbc95506ea5bd87357ff1d7d8d21facb8
Ray Zhang <rayz@codeaurora.org>
2015-11-25 13:58:56 +0800

    healthd: charger: allow turn on/off backlight in power off charging
    On some targets the gr_fb_blank does not control the backlight.Thus
    manually turn on/off backlight with the gr_fb_blank call.
    
    Change-Id: Id92bc3cacc80efa3b5a0a5312d77934c08529bc2
    Signed-off-by: Ray Zhang <rayz@codeaurora.org>

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -69,6 +69,7 @@ char *locale;
 
 #define BATTERY_FULL_THRESH     95
 
+#define BACKLIGHT_PATH          "/sys/class/leds/lcd-backlight/brightness"
 #define LAST_KMSG_PATH          "/proc/last_kmsg"
 #define LAST_KMSG_PSTORE_PATH   "/sys/fs/pstore/console-ramoops"
 #define LAST_KMSG_MAX_SZ        (32 * 1024)
@@ -174,6 +175,39 @@ static struct android::BatteryProperties *batt_prop;
 static int char_width;
 static int char_height;
 static bool minui_inited;
+
+void healthd_set_backlight(bool en);
+
+#define BACKLIGHT_ON_LEVEL    100
+#define BACKLIGHT_OFF_LEVEL    0
+void healthd_set_backlight(bool en)
+{
+    int fd;
+    char buffer[10];
+
+    if (access(BACKLIGHT_PATH, R_OK | W_OK) != 0)
+        LOGW("Backlight control not support\n");
+
+    memset(buffer, '\0', sizeof(buffer));
+    fd = open(BACKLIGHT_PATH, O_RDWR);
+    if (fd < 0) {
+        LOGE("Could not open backlight node : %s\n", strerror(errno));
+        goto cleanup;
+    }
+    LOGV("set backlight status to %d\n", en);
+    if (en)
+        snprintf(buffer, sizeof(buffer), "%d\n", BACKLIGHT_ON_LEVEL);
+    else
+        snprintf(buffer, sizeof(buffer), "%d\n", BACKLIGHT_OFF_LEVEL);
+
+    if (write(fd, buffer,strlen(buffer)) < 0) {
+        LOGE("Could not write to backlight node : %s\n", strerror(errno));
+        goto cleanup;
+    }
+cleanup:
+    if (fd >= 0)
+        close(fd);
+}
 
 /* current time in milliseconds */
 static int64_t curr_time_ms(void)
