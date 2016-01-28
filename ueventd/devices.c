4013dff72a07f70381bf356e5d9df19230daaec8
guyang <guyang@codeaurora.org>
2016-01-28 19:12:28 +0800

    Revert "patch for u-disk automatically effect side depends-on:1511628 Change-Id: I2723b116cb9b34f20317ebcd9ff30a50c8786ee4"
    This reverts commit 3cc35e03f421ad3e408da70832e58a571fc11ff9
    Change-Id: Ia7626579e72b6a3c9e169d20b433bc50e557560c

--- ueventd/devices.c
+++ ueventd/devices.c
@@ -261,7 +261,6 @@ static int read_from_file(const char *path, char *buf, ssize_t count)
 /* 1: in,  0: out */
 void handle_sd_plug_in_out(int in_out)
 {
-#if 1
     int i;
     struct stat info;
     char emp_str[2] = " ";
@@ -280,7 +279,6 @@ void handle_sd_plug_in_out(int in_out)
 		i = write_file(block_path, emp_str);
 		//if (i < 0 ) printf("remove sd failed, ret =%d\n", i);
     }
-#endif
 }
 
 static void handle_device_event(struct uevent *uevent)
@@ -481,7 +479,7 @@ void handle_device_fd()
         parse_event(msg, &uevent);
 
         handle_device_event(&uevent);
-        //handle_firmware_event(&uevent);
+        handle_firmware_event(&uevent);
     }
 }
 
@@ -549,7 +547,7 @@ void device_init(void)
 
     fcntl(device_fd, F_SETFD, FD_CLOEXEC);
     fcntl(device_fd, F_SETFL, O_NONBLOCK);
-#if 0
+
     if (stat(coldboot_done, &info) < 0) {
         t0 = get_usecs();
         coldboot("/sys/class");
@@ -562,7 +560,6 @@ void device_init(void)
     } else {
         log_event_print("skipping coldboot, already done\n");
     }
-#endif
 }
 
 int get_device_fd()
