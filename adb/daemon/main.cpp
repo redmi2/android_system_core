8eb2f9592a2fe77b0f5dd83c0e9dbe994c0a7f28
Tom Marshall <tdm.code@gmail.com>
2017-08-23 22:11:03 +0000

    adb: Look for executables in alternate places
     * Restore global variable recovery_mode.  This is set based on the
       commandline parameter --banner.
    
     * Use /sbin for path to 'sh' and 'bu' when in recovery mode.
    
     * Honor persist.sys.adb.shell when not in recovery mode.
    
    Change-Id: I02ac651397021097fe865436c982e2b720d7917f

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -221,6 +221,8 @@ int adbd_main(int server_port) {
     return 0;
 }
 
+int recovery_mode = 0;
+
 int main(int argc, char** argv) {
     while (true) {
         static struct option opts[] = {
@@ -252,6 +254,8 @@ int main(int argc, char** argv) {
             return 1;
         }
     }
+
+    recovery_mode = (strcmp(adb_device_banner, "recovery") == 0);
 
     close_stdin();
 
